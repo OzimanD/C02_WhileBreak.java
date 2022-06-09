14765840d92d6a7dbb22300cdb4160bcab9d344a
OzimanD <89782267+OzimanD@users.noreply.github.com>
2022-06-09 19:30:17 +0300

    get

---
+++ src/Test.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+
+for(int i = 10; i>0;i--){
+    System.out.println(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        for (int i = a; i<=b;i++){
+//            System.out.println(i);
+//            c+=1;
+//        }
+//        System.out.println(c);
+//        int a =7;
+//        for (int i = a; i>=b;i--){
+//            System.out.println(i);
+//        }
+//        double result = 1;
+//        for (int i=1; i<=b; i++){
+//            result = result*A;
+//            System.out.println(result);
+//        }
+    }
+}
